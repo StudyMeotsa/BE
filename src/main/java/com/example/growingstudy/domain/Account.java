d388f04a7c690c57de63c31b55dac57df34fa571
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-10-10 00:40:21 +0900

    feat. 계정 Account 클래스 추가 (#1)
    클래스의 필드들은 대략적인 것으로, 추후 변경될 수 있음

---
+++ src/main/java/com/example/growingstudy/domain/Account.java
@@ -0,0 +1,49 @@
+package com.example.growingstudy.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Account")
+public class Account {
+
+    // 필드들은 대략적인 것으로, 나중에 변경될 수 있음
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; // H2에서 AtomicLong 오류 나서 long으로 뒀음
+    private String username;
+    private String password;
+    private String nickname;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
