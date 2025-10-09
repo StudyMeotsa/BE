1735679b6d041833d5da0f706043251b8ad8162d
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-10-10 04:21:03 +0900

    chore. SecurityConfig 위치 변경 (#1)

--- src/main/java/com/example/growingstudy/security/config/SecurityConfig.java
+++ src/main/java/com/example/growingstudy/security/config/SecurityConfig.java
@@ -1,8 +1,7 @@
-package com.example.growingstudy.security;
+package com.example.growingstudy.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
