a7dec8c5ff150159d2be997337ed4c6c76a90346
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-12-07 17:52:57 +0900

    refactor: 비밀번호 확인 일치 constraint

---
+++ src/main/java/com/example/growingstudy/auth/validator/PasswordConfirmConstraintValidator.java
@@ -0,0 +1,14 @@
+package com.example.growingstudy.auth.validator;
+
+import com.example.growingstudy.auth.constraint.PasswordConfirmConstraint;
+import com.example.growingstudy.auth.dto.RegisterRequestDto;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordConfirmConstraintValidator implements ConstraintValidator<PasswordConfirmConstraint, RegisterRequestDto> {
+
+    @Override
+    public boolean isValid(RegisterRequestDto value, ConstraintValidatorContext context) {
+        return value.getPasswordConfirm().equals(value.getPassword());
+    }
+}
