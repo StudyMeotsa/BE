0e05f3eb405aba4cdf8a5a7d6129fa8537f4f5ec
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-12-10 17:48:23 +0900

    refactor: 로그인 성공 핸들러에서 서비스를 통해 토큰 생성

--- src/main/java/com/example/growingstudy/security/handler/LoginSuccessHandler.java
+++ src/main/java/com/example/growingstudy/security/handler/LoginSuccessHandler.java
@@ -1,5 +1,8 @@
 package com.example.growingstudy.security.handler;
 
+import com.example.growingstudy.security.dto.JwtResponseDto;
+import com.example.growingstudy.security.entity.UserDetailsWithId;
+import com.example.growingstudy.security.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,10 +13,22 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import java.io.IOException;
 
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+
+    public LoginSuccessHandler(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    // 서비스를 통해 토큰을 생성하여 응답 반환
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-        request.setAttribute("username", authentication.getName());
-        chain.doFilter(request, response);
+        UserDetailsWithId userDetails = (UserDetailsWithId) authentication.getPrincipal();
+        long userId = userDetails.getUserId();
+
+        JwtResponseDto jwtResponseDto = jwtService.generateTokens(userId);
+
+        // TODO: 응답에 객체 쓰기
     }
 
     @Override
