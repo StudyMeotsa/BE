987e61d969abe4057b44b8742d2531b148f9c3a1
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-12-13 04:24:26 +0900

    refactor: auth 패키지 내 컨트롤러와 서비스 내 불필요 메소드 제거

--- src/main/java/com/example/growingstudy/auth/controller/AuthController.java
+++ src/main/java/com/example/growingstudy/auth/controller/AuthController.java
@@ -1,7 +1,5 @@
 package com.example.growingstudy.auth.controller;
 
-import com.example.growingstudy.auth.dto.JwtResponseDto;
-import com.example.growingstudy.auth.dto.RefreshOrLogoutRequestDto;
 import com.example.growingstudy.auth.dto.RegisterRequestDto;
 import com.example.growingstudy.auth.service.AuthService;
 import org.slf4j.Logger;
@@ -24,15 +22,6 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<JwtResponseDto> login(@RequestAttribute("username") String username) {
-        logger.debug("로그인 후 토큰 발급 처리 시작");
-        JwtResponseDto body = authService.generateJwtToken(username);
-        logger.debug("로그인 후 토큰 발급 처리 성공");
-        logger.info("유저 {}가 로그인", username);
-        return ResponseEntity.accepted().body(body);
-    }
-
     @PostMapping("/register")
     public ResponseEntity<Void> register(@Validated @RequestBody RegisterRequestDto request) {
         logger.debug("회원가입 요청 처리 시작");
@@ -40,20 +29,5 @@ public class AuthController {
         authService.register(request);
         logger.info("유저 {}가 회원가입", request.getUsername());
         return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    @PostMapping("/refresh")
-    public ResponseEntity<JwtResponseDto> refreshTokens(@RequestBody RefreshOrLogoutRequestDto request) {
-        logger.debug("토큰 재발급 처리 시작");
-        String refreshString = request.getRefreshToken();
-        JwtResponseDto body = authService.refreshTokens(refreshString);
-        logger.debug("토큰 재발급 처리 성공");
-        return ResponseEntity.accepted().body(body);
-    }
-
-    // 로그아웃 엔드포인트: 실제로 이 메소드가 호출되진 않으나, 일관성 및 Swagger 위해 추가
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(@RequestBody RefreshOrLogoutRequestDto request) {
-        return ResponseEntity.accepted().build();
     }
 }
--- src/main/java/com/example/growingstudy/auth/service/AuthService.java
+++ src/main/java/com/example/growingstudy/auth/service/AuthService.java
@@ -1,6 +1,5 @@
 package com.example.growingstudy.auth.service;
 
-import com.example.growingstudy.auth.dto.JwtResponseDto;
 import com.example.growingstudy.auth.entity.Account;
 import com.example.growingstudy.auth.dto.RegisterRequestDto;
 import com.example.growingstudy.auth.enums.RegisterFailedType;
@@ -10,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,13 +19,10 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final AccountRepository accountRepository;
 
-    private final JwtService jwtService;
-
     @Autowired
-    public AuthService(PasswordEncoder passwordEncoder, AccountRepository accountRepository, JwtService jwtService) {
+    public AuthService(PasswordEncoder passwordEncoder, AccountRepository accountRepository) {
         this.passwordEncoder = passwordEncoder;
         this.accountRepository = accountRepository;
-        this.jwtService = jwtService;
     }
 
     @Transactional
@@ -58,37 +53,5 @@ public class AuthService {
             logger.debug("이미 해당 유저가 존재");
             throw new RegisterFailedException(RegisterFailedType.USERNAME_NOT_UNIQUE);
         }
-    }
-
-    public JwtResponseDto generateJwtToken(String username) {
-        logger.debug("JWT 토큰 발급 시작");
-        logger.trace("액세스 토큰을 생성");
-        Jwt accessToken = jwtService.generateAccessToken(username);
-        logger.trace("리프레쉬 토큰을 생성");
-        Jwt refreshToken = jwtService.generateRefreshToken(username);
-
-        logger.trace("토큰을 담을 응답 객체 생성");
-        JwtResponseDto jwtResponseDto = new JwtResponseDto();
-        jwtResponseDto.setAccessToken(accessToken.getTokenValue());
-        jwtResponseDto.setRefreshToken(refreshToken.getTokenValue());
-
-        logger.debug("JWT 토큰 발급 성공");
-
-        return jwtResponseDto;
-    }
-
-    public JwtResponseDto refreshTokens(String refreshToken) {
-        logger.debug("리프레쉬 토큰 사용");
-        String username = jwtService.consumeRefreshToken(refreshToken);
-        logger.debug("리프레쉬 토큰 사용 완료");
-
-        return generateJwtToken(username);
-    }
-
-    // 로그아웃: 현재 리프레쉬 토큰을 목록에서 제거
-    public void logout(String refreshToken) {
-        logger.debug("로그아웃 처리");
-        jwtService.consumeRefreshToken(refreshToken);
-        logger.debug("로그아웃 완료");
     }
 }
