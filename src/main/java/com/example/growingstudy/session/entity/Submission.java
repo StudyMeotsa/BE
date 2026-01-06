7b055901d58042665c5b540adf02d763da72182f
rattyrat00 <dakyoung2451@gmail.com>
2026-01-06 17:21:00 +0900

    fix : submission entity edit

--- src/main/java/com/example/growingstudy/session/entity/Submission.java
+++ src/main/java/com/example/growingstudy/session/entity/Submission.java
@@ -2,10 +2,7 @@ package com.example.growingstudy.session.entity;
 
 import com.example.growingstudy.studygroup.entity.GroupMember;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -18,14 +15,14 @@ public class Submission {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // id BIGINT (PK)
 
-    @Column(name = "content", columnDefinition = "TEXT")
-    private String content; // content TEXT (제출 내용)
+    @Column(name = "content", length = 2000)
+    private String content; // content VARCHAR(2000)
 
     @Column(name = "image_path")
     private String imagePath; // image_path VARCHAR(255)
 
     @Column(name = "is_verified", nullable = false)
-    private boolean isVerified = false; // is_verified BOOLEAN (Default False)
+    private boolean isVerified = false; // is_verified BOOLEAN
 
     @Column(name = "submitted_at")
     private LocalDateTime submittedAt; // submitted_at DATETIME
@@ -36,18 +33,19 @@ public class Submission {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "submitter_id", nullable = false)
-    private GroupMember submitter; // submitter_id BIGINT (FK: group_member)
+    private GroupMember submitter; // submitter_id BIGINT (FK)
 
     @PrePersist
     public void prePersist() {
-        this.submittedAt = LocalDateTime.now(); // 제출 시간 자동 생성
+        this.submittedAt = LocalDateTime.now();
     }
 
+    @Builder
     public Submission(String content, String imagePath, Checklist checklist, GroupMember submitter) {
         this.content = content;
         this.imagePath = imagePath;
+        this.isVerified = false; 
         this.checklist = checklist;
         this.submitter = submitter;
-        this.isVerified = false;    // 제출 시 초기값은 false
     }
 }
