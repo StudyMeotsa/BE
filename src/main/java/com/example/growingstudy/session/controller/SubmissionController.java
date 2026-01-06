08f2e2de292fbd5563f00137eae5a51669c59bec
rattyrat00 <dakyoung2451@gmail.com>
2026-01-06 20:55:16 +0900

    refactor : submission controller

--- src/main/java/com/example/growingstudy/session/controller/SubmissionController.java
+++ src/main/java/com/example/growingstudy/session/controller/SubmissionController.java
@@ -1,9 +1,10 @@
 package com.example.growingstudy.session.controller;
 
 import com.example.growingstudy.session.dto.SubmissionCreateDto;
-import com.example.growingstudy.session.entity.Submission;
+import com.example.growingstudy.session.dto.SubmissionResponseDto;
 import com.example.growingstudy.session.service.SubmissionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +17,16 @@ public class SubmissionController {
     private final SubmissionService submissionService;
 
     @PostMapping
-    public Submission createSubmission(
-            @PathVariable Long checklistId, 
+    public ResponseEntity<Long> createSubmission(
+            @PathVariable Long checklistId,
             @RequestBody SubmissionCreateDto dto) {
-        return submissionService.createSubmission(checklistId, dto);
+        Long id = submissionService.createSubmission(checklistId, dto);
+        return ResponseEntity.ok(id);
     }
 
     @GetMapping
-    public List<Submission> getSubmissions(@PathVariable Long checklistId) {
-        return submissionService.getSubmissionsByChecklist(checklistId);
+    public ResponseEntity<List<SubmissionResponseDto>> getSubmissions(@PathVariable Long checklistId) {
+    List<SubmissionResponseDto> response = submissionService.getSubmissionsByChecklist(checklistId);
+    return ResponseEntity.ok(response);
     }
 }
