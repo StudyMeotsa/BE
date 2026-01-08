30f14f12b318a6072b04798e29264f3013d91682
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-01-09 04:59:12 +0900

    feat: 그룹의 커피 현황 관련 레포지토리

---
+++ src/main/java/com/example/growingstudy/coffee/repository/GroupCoffeeRepository.java
@@ -0,0 +1,7 @@
+package com.example.growingstudy.coffee.repository;
+
+import com.example.growingstudy.coffee.entity.GroupCoffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupCoffeeRepository extends JpaRepository<GroupCoffee, Long> {
+}
