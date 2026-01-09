34d74e6be40285eec3895e8bdab437d0031bb49c
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-01-09 16:47:50 +0900

    chore: 체크리스트 전체 보기 응답에 커피 현황 다시 추가

--- src/main/java/com/example/growingstudy/session/dto/ChecklistOverviewResponse.java
+++ src/main/java/com/example/growingstudy/session/dto/ChecklistOverviewResponse.java
@@ -1,10 +1,12 @@
 package com.example.growingstudy.session.dto;
 
+import com.example.growingstudy.coffee.dto.GroupCoffeeProgressDto;
+
 import java.util.List;
 
 public record ChecklistOverviewResponse(
         SessionInfoResponse session,
-//        GroupCoffeeProgressDto coffee,    //coffee.dto에 넣어뒀음
+        GroupCoffeeProgressDto coffee,
         List<ChecklistStatusDto> checklists
 ) {
 }
