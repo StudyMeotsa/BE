e61991ec4de8029f0e7f51ac7f1f8fa63bc4da67
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-01-09 16:35:28 +0900

    feat: DB에서 특정 그룹의 커피 현황 조회

--- src/main/java/com/example/growingstudy/coffee/repository/GroupCoffeeRepository.java
+++ src/main/java/com/example/growingstudy/coffee/repository/GroupCoffeeRepository.java
@@ -2,6 +2,17 @@ package com.example.growingstudy.coffee.repository;
 
 import com.example.growingstudy.coffee.entity.GroupCoffee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface GroupCoffeeRepository extends JpaRepository<GroupCoffee, Long> {
+
+    /**
+     * 해당 그룹의 커피 현황 조회
+     * @param groupId 조회할 그룹의 id
+     * @return 그룹의 커피 현황
+     */
+    @Query("select gc from GroupCoffee gc where gc.group.id = :groupId")
+    Optional<GroupCoffee> findByGroupId(Long groupId);
 }
