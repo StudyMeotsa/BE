8aa2058a84aca16a70b7bb28bc915be8c853b638
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-01-10 04:50:26 +0900

    chore: 내 정보 보기 및 내 커피 도감에서도 액세스 토큰 여부 확인

--- src/main/java/com/example/growingstudy/security/filter/CheckAccessTokenFilter.java
+++ src/main/java/com/example/growingstudy/security/filter/CheckAccessTokenFilter.java
@@ -9,10 +9,12 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 // 토큰이 액세스 타입의 토큰인지 확인함
 public class CheckAccessTokenFilter extends OncePerRequestFilter {
     private final String authURIPrefix = "/api/auth";
+    private final List<String> uriToExclude = List.of("/me", "/mycoffees");
     private final JwtService jwtService;
 
     public CheckAccessTokenFilter(JwtService jwtService) {
@@ -22,7 +24,9 @@ public class CheckAccessTokenFilter extends OncePerRequestFilter {
     // 토큰 재발급, 로그인, 로그아웃, 회원가입에 대해 미적용
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        return request.getRequestURI().startsWith(authURIPrefix);
+        // 내 정보 확인, 커피 도감은 필터 적용 대상
+        return request.getRequestURI().startsWith(authURIPrefix) &&
+                uriToExclude.stream().noneMatch(uri -> request.getRequestURI().endsWith(uri));
     }
 
     @Override
